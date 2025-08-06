afbeeb6d47643697b34e14dfcf0e82ef20a455fc
RoshniSahoo30 <127582340+RoshniSahoo30@users.noreply.github.com>
2025-08-06 19:13:40 +0530

    Time: 0 ms (100%), Space: 47.8 MB (81.11%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int hIndex(int[] nums) {
+
+        int l=0,r=nums.length-1,ans=0,c=0;
+        while(l<=r)
+        {
+            int mid=l+(r-l)/2;
+            if(nums[mid]==nums.length-mid)
+            return nums[mid];
+            else if(nums[mid]<nums.length-mid)
+            l=mid+1;
+            else
+            r=mid-1;
+        }
+        return nums.length-l;
+    }
+}
