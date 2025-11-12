f7cf2ed66300dca91f8717dc78ef696b05e041b2
RoshniSahoo30 <127582340+RoshniSahoo30@users.noreply.github.com>
2025-11-12 23:02:46 +0530

    Time: 4 ms (70.99%), Space: 44.8 MB (47.69%) - LeetHub

--- 4115-minimum-distance-between-three-equal-elements-i/4115-minimum-distance-between-three-equal-elements-i.java
+++ 4115-minimum-distance-between-three-equal-elements-i/4115-minimum-distance-between-three-equal-elements-i.java
@@ -1,17 +1,27 @@
 class Solution {
     public int minimumDistance(int[] nums) {
-        int ans = Integer.MAX_VALUE;
-        for(int i = 0; i < nums.length - 2; i++) {
-            for(int j = i + 1; j < nums.length - 1; j++) {
-                if(nums[j] != nums[i]) continue;
-                for(int k = j + 1; k < nums.length; k++) {
-                    if(nums[j] == nums[k]) {
-                        ans = Math.min(ans , (j - i) + (k - j) + (k - i));
-                    }
+
+        int n = nums.length;
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            map.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
+        }
+        int minDistance = Integer.MAX_VALUE;
+        boolean found = false;
+        for (Map.Entry<Integer, List<Integer>> entry : map.entrySet()) {
+            List<Integer> indices = entry.getValue();
+            if (indices.size() >= 3) {
+                for (int j = 0; j + 2 < indices.size(); j++) {
+                    int first = indices.get(j);
+                    int third = indices.get(j + 2);
+                    int distance = 2 * (third - first);
+                    minDistance = Math.min(minDistance, distance);
+                    found = true;
                 }
             }
         }
-        if(ans == Integer.MAX_VALUE) return -1;
-        return ans;
+
+        return found ? minDistance : -1;
+        
     }
 }
